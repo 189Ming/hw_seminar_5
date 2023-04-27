6979dfb3fdff2d85b531e0f2a9b33d9e398756f3
Dmitry <st1makk@gmail.com>
2023-04-27 21:17:22 +0700

    34, 36 done

---
+++ 36/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length -1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int SumElementsNegativePos(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(6, -20, 20);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+
+int sumElementsNegativePos = SumElementsNegativePos(array);
+Console.WriteLine(sumElementsNegativePos);
+
+// не стал заполнять случайными числами, взял интервал. Огромные значения получаются, неудобно проверять.
